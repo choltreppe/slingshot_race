b71cb3fb2c042037b198f5e919d5bfd43a857ead
Joel Lienhard <joel@mclien.de>
2025-01-06 18:06:53 +0100

    stuff

--- src/game.nim
+++ src/game.nim
@@ -4,12 +4,12 @@ import ./prelude, ./queue
 
 
 const
-  shipAccel = 0.15
-  shipFuelUse = 0.1
+  shipAccel = 0.2
+  shipFuelUse = 0.14
   
   planetRadiusRange = 0.04 .. 0.12
   planetsMinMargin = 0.1
-  planetMaxDistance = 0.4
+  planetMaxDistance = 0.6
   planetDistanceRange = planetRadiusRange.a+planetsMinMargin*1.1 .. planetMaxDistance+planetsMinMargin
 
   gravityStrength = 5
@@ -23,13 +23,14 @@ type
     position, velocity: Vec2
     fuel: float32 = 1
 
-  Planet = object
+  SpaceObjectKind = enum planet, refuel
+  SpaceObject = object
     position: Vec2
-    radius: float32
-
-  FuelOrb = object
-    position: Vec2
-    fuel: float32
+    case kind: SpaceObjectKind
+    of planet:
+      radius: float32
+    of refuel:
+      discard
 
 var
   camera: Camera2D
@@ -37,10 +38,8 @@ var
   shipMinY: float32
 
   ship: Ship
-  space = (
-    planets: newQueue[Planet](cap = 16),
-    fuelOrbs: newQueue[FuelOrb](cap = 10),
-  )
+  spaceObjects = newQueue[SpaceObject](cap = 16)
+  lastRefuelOrbDistance: int32
   score: uint64
   scoreStep: float32
   cameraShake: tuple[
@@ -50,8 +49,13 @@ var
 
 randomize()
 
+func newPlanet*(position: Vec2, radius: float32): SpaceObject =
+  SpaceObject(kind: planet, position: position, radius: radius)
 
-func radius(orb: FuelOrb): float32 = orb.fuel * 0.02
+func boundingRadius*(obj: SpaceObject): float32 =
+  case obj.kind
+  of planet: obj.radius
+  of refuel: 0.02
 
 proc initGame* =
   let screenSize = vec2(float32 getScreenWidth(), float32 getScreenHeight())
@@ -60,12 +64,12 @@ proc initGame* =
   shipMinY = viewHeight * 0.66
 
   ship = Ship(position: vec2(0.5, shipMinY))
-  for objs in space.fields:
-    clear objs
-  space.planets &= Planet(
-    position: vec2(rand(1f32), 0),
-    radius: rand(planetRadiusRange)
+  clear spaceObjects
+  spaceObjects &= newPlanet(
+    vec2(rand(1f32), 0),
+    rand(planetRadiusRange)
   )
+  lastRefuelOrbDistance = 0
   score = 0
   scoreStep = 0
 
@@ -76,10 +80,11 @@ proc drawGame* =
     clearBackground(Black)
     drawText($score, 340, 10, 30, White)
     mode2D(camera):
-      for planet in space.planets:
-        drawCircleLines(planet.position, planet.radius, White)
-      for orb in space.fuelOrbs:
-        drawCircle(orb.position, orb.radius, Blue)
+      for obj in spaceObjects:
+        drawCircleLines(obj.position, obj.boundingRadius):
+          case obj.kind
+          of planet: White
+          of refuel: Blue
       drawCircle(ship.position, 0.01, White)
 
       block drawFuelBar:
@@ -92,8 +97,13 @@ proc drawGame* =
         rect.width *= ship.fuel
         drawRectangle(rect, White)
 
+iterator planets: SpaceObject =
+  for obj in spaceObjects:
+    if obj.kind == planet:
+      yield obj
+
 proc gravityAt(position: Vec2): Vec2 =
-  for planet in space.planets:
+  for planet in planets():
     let v = planet.position - position
     let distance = length(v)
     let direction = normalize(v)
@@ -102,7 +112,7 @@ proc gravityAt(position: Vec2): Vec2 =
 
 proc distanceToPlanets(position: Vec2): float32 =
   result = high(float32)
-  for planet in space.planets:
+  for planet in planets():
     let d = dist(position, planet.position) - planet.radius
     if d < result:
       result = d
@@ -122,11 +132,11 @@ proc updateShip(dt: float32) =
         ship.fuel = max(0, ship.fuel - shipFuelUse*dt)
 
 proc addPlanet =
-  let prevPlanet = space.planets.last
-  let distance = prevPlanet.radius + rand(planetDistanceRange)
+  let prevObj = spaceObjects.last
+  let distance = prevObj.boundingRadius + rand(planetDistanceRange)
 
   proc getMaxAngle(borderX: float32): float32 =
-    let margin = abs(borderX - prevPlanet.position.x)
+    let margin = abs(borderX - prevObj.position.x)
     if margin > distance: Pi/2
     else:
       Pi/2 - arccos(margin/distance)
@@ -134,62 +144,65 @@ proc addPlanet =
   let angleRange =
     if rand(1f32) < 0.4:
       -getMaxAngle(1) .. getMaxAngle(0)
-    elif prevPlanet.position.x < 0.5:
+    elif prevObj.position.x < 0.5:
       -getMaxAngle(1) .. 0f32
     else:
       0f32 .. getMaxAngle(0)
 
-  var newPlanet: Planet
+  let newObjectKind =
+    if rand(1f32) < 0.2: refuel
+    else: planet
+
   while true:
-    let position = prevPlanet.position + rotate(rand(angleRange)) * vec2(0, -distance)
+    let position = prevObj.position + rotate(rand(angleRange)) * vec2(0, -distance)
     let maxRadius = min(position.distanceToPlanets - planetsMinMargin, -(position.y + planetRadiusRange.a))
     if maxRadius > planetRadiusRange.a:
-      newPlanet = Planet(
-        position: position,
-        radius: rand(planetRadiusRange.a .. min(planetRadiusRange.b, maxRadius))
-      )
-      space.planets &= newPlanet
+      spaceObjects.add:
+        if lastRefuelOrbDistance > 3 and rand(6 - lastRefuelOrbDistance) == 0:
+          lastRefuelOrbDistance = 0
+          SpaceObject(kind: refuel, position: position)
+        else:
+          inc lastRefuelOrbDistance
+          newPlanet(
+            position,
+            rand(planetRadiusRange.a .. min(planetRadiusRange.b, maxRadius))
+          )
       break
-
-  let surfaceDistance = distance - prevPlanet.radius - newPlanet.radius
-  if surfaceDistance < 2*planetsMinMargin:
-    let position = newPlanet.position + normalize(prevPlanet.position - newPlanet.position) * (newPlanet.radius + surfaceDistance/2)
-    if position.x in 0.3 .. 0.7:
-      space.fuelOrbs &= FuelOrb(position: position, fuel: 1)
 
 proc updateGame*(dt: float32) =
   updateShip(dt)
   if (let d = shipMinY - ship.position.y; d > 0):
     ship.position.y = shipMinY
-    for objs in space.fields:
-      for obj in objs.mitems:
-        obj.position.y += d
+    for obj in spaceObjects.mitems:
+      obj.position.y += d
 
     scoreStep += d
     if scoreStep > 1:
       inc score
       scoreStep -= 1
 
-  for orb in space.fuelOrbs:
-    if dist(ship.position, orb.position) < orb.radius+0.01:
+  for obj in spaceObjects:
+    if obj.kind == refuel and dist(ship.position, obj.position) < 0.04:
       ship.fuel = 1
       break
   
-  if space.planets.last.position.y > -planetRadiusRange.a:
+  if spaceObjects.last.position.y > -planetRadiusRange.a:
     addPlanet()
-  for objs in space.fields:
-    if (
-      (Some(@first) ?= objs.first) and
-      first.position.y > viewHeight + first.radius
-    ):
-      objs.deleteHead()
+  if (
+    (Some(@first) ?= spaceObjects.first) and
+    first.position.y > viewHeight + first.boundingRadius
+  ):
+    spaceObjects.deleteHead()
 
   block:
     let distance = ship.position.distanceToPlanets
     if distance < 0 or ship.position.x notin -0.1..1.1 or ship.position.y > viewHeight:
-      quit 0
+      initGame()
     elif distance < cameraShakeDistance:
       cameraShake.radius = ((cameraShakeDistance - distance) / cameraShakeDistance)^2 * cameraShakeStrength
+      debugEcho cameraShake.radius
       if length(camera.target) > cameraShake.radius:
-        cameraShake.direction = rotate(rand(2*Pi).float32) * vec2(1, 0) - camera.target
+        cameraShake.direction = normalize(rotate(rand(2*Pi).float32) * vec2(cameraShake.radius, 0) - camera.target)
       camera.target += cameraShake.direction * dt * (cameraShake.radius / cameraShakeStrength)
+    else:
+      camera.target = vec2(0)  # just to be sure
